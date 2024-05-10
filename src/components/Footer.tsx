d1d53b05f5bfc9651aa043b61a6fe59b03cfeb07
Mirza Mesinovic <mirza.mesinovic@chasacademy.se>
2024-05-10 22:07:01 +0200

    Footer resizing take 4

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,24 +8,24 @@ export default function Footer({}: Props) {
     <div className={css.footer_container}>
       <ul className={css.ul}>
         <li>
-          <Icon.User color="var(--blacker)" className={css.icon_standard} />
+          <Icon.User color="var(--white)" className={css.icon_standard} />
           <p>Profile</p>
         </li>
         <li>
-          <Icon.Clock color="var(--blacker)" className={css.icon_standard} />
+          <Icon.Clock color="var(--white)" className={css.icon_standard} />
           <p>History</p>
         </li>
         <li>
-          <Icon.Plus color="var(--blacker)" className={css.icon_standard} />
+          <Icon.Plus color="var(--white)" className={css.icon_standard} />
           <p>Workout</p>
         </li>
         <li>
-          <Icon.PieChart color="var(--blacker)" className={css.icon_standard} />
+          <Icon.PieChart color="var(--white)" className={css.icon_standard} />
           <p>Progress</p>
         </li>
         <li>
           <Icon.Thermometer
-            color="var(--blacker)"
+            color="var(--white)"
             className={css.icon_standard}
           />
           <p>Measures</p>
